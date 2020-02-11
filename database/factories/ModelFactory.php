e9802b2cb655fa4fb7852f3f2f99d6a9374b71d4
Edward Osei-Nyarko <edward.osei.nyarko@gmail.com>
2020-02-11 11:38:20 +0000

    Factory for Product Model added

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Category;
+use App\Product;
 use App\User;
 use Faker\Generator as Faker;
 
@@ -32,5 +33,17 @@ $factory->define(Category::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->paragraph(1),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+        'seller_id' => User::all()->random()->id,
     ];
 });
