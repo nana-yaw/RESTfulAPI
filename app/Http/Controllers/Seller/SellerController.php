fbea87054c4417ff7e593b85a6e09cb1a46ca10a
Edward Osei-Nyarko <edward.osei.nyarko@gmail.com>
2020-02-12 15:04:59 +0000

    Generalizing the response methods

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Seller;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Seller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
 
-class SellerController extends Controller
+class SellerController extends ApiController
 {
     /**
      * Display a listing of the resource.
