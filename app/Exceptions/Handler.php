6da7ce4c831c3031b7600051dc83707da8627945
Edward Osei-Nyarko <edward.osei.nyarko@gmail.com>
2020-02-12 16:40:10 +0000

    handled not found http exception response as json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,6 +9,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -70,6 +71,10 @@ class Handler extends ExceptionHandler
 
         if ($exception instanceof AuthorizationException) {
             return $this->errorResponse($exception->getMessage(), 403);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse('The specified URL cannot be found', 404);
         }
 
         return parent::render($request, $exception);
