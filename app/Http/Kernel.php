7f584f3cbe63b4fba5e7203546cc6dbc2b95a05a
Edward Osei-Nyarko <edward.osei.nyarko@gmail.com>
2020-03-05 16:26:49 +0000

    Added custom middleware to add app name to API response headers

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            'signature:X-Application-Name',
             'throttle:60,1',
             'bindings',
         ],
@@ -57,5 +58,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'signature' => \App\Http\Middleware\SignatureMiddleware::class,
     ];
 }
