1aff9e56bfc50bdca1e8391fd4378af543de2935
Edward Osei-Nyarko <edward.osei.nyarko@gmail.com>
2020-03-17 13:43:46 +0000

    Added sorting and filtering responses

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -18,12 +18,27 @@ class UserTransformer extends TransformerAbstract
             'identifier' => (int)$user->id,
             'name' => (string)$user->name,
             'email' => (string)$user->email,
-            'email' => (string)$user->email,
             'isVerified' => (int)$user->verified,
             'isAdmin' => ($user->admin === 'true'),
             'creationDate' => (string) $user->created_at,
             'lastChange' => (string) $user->updated_at,
             'deletedDate' => isset($user->deleted_at) ? (string) $user->deleted_at : null,
         ];
+    }
+
+    public static function originalAttribute($index)
+    {
+            $attributes = [
+                'identifier' => 'id',
+                'name' => 'name',
+                'email' => 'email',
+                'isVerified' => 'verified',
+                'isAdmin' => 'admin',
+                'creationDate' => 'created_at',
+                'lastChange' => 'updated_at',
+                'deletedDate' => 'deleted_at',
+            ];
+
+            return isset($attributes[$index]) ? $attributes[$index] : null;
     }
 }
