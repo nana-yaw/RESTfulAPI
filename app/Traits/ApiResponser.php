84315d27f2749d094a46c068579f4904a51e5386
Edward Osei-Nyarko <edward.osei.nyarko@gmail.com>
2020-03-18 10:30:07 +0000

    Added caching system

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -5,6 +5,7 @@ namespace App\Traits;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
 
 trait ApiResponser
@@ -34,6 +35,8 @@ trait ApiResponser
         $collection = $this->paginate($collection);
 
         $collection = $this->transformData($collection, $transformer);
+        
+        $collection = $this->cacheResponse($collection);
 
         return $this->successResponse($collection, $code);
     }
@@ -109,5 +112,14 @@ trait ApiResponser
         $transformation = fractal($data, new $transformer);
 
         return $transformation->toArray();
+    }
+
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+
+        return Cache::remember($url, 30/60, function() use ($data) {
+            return $data;
+        });
     }
 }
