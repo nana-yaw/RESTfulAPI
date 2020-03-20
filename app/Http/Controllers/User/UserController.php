aa5ddb9fbfd474928e55b0c8d36a69da3b1db6af
Edward Osei-Nyarko <edward.osei.nyarko@gmail.com>
2020-03-20 16:11:18 +0000

    Added client credentials in User Controller

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -14,7 +14,6 @@ class UserController extends ApiController
 
     public function __construct()
     {
-        // parent::__construct();
 
         $this->middleware('client.credentials')->only(['store','resend']);
 
